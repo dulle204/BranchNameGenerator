65266e944a8b566bead1e35a8f274227c94248b8
Dusan Ivanisevic <d.ivanisevic@levi9.com>
2019-09-27 11:22:38 +0200

    initial commit

---
+++ BranchNameCreator/BranchNameCreator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchNameCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BranchNameCreator/BranchNameCreatorApp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BranchNameCreatorApp
+{
+    public partial class BranchNameGenerator : Form
+    {
+        public BranchNameGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = 
+                richTextBox2
+                .Text
+                .Trim()
+                .Replace(' ', '_')
+                .Replace('\n', '-');
+
+            Clipboard.SetText(richTextBox1.Text);
+        }
+    }
+}
